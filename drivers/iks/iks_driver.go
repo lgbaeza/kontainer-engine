ef9cf7f121bac3350482e5a95c318f1d30f0300f
Luis Gerardo Baeza Castro <luis_v8k@hotmail.com>
2018-09-29 20:54:37 -0500

    ibm kubernetes service structure driver structure

--- drivers/iks/iks_driver.go
+++ drivers/iks/iks_driver.go
@@ -2,3 +2,4 @@ package iks
 //working on the implementation of IBM Container Service for Rancher
 //@lgbaeza
 //https://us-south.containers.bluemix.net/swagger-api/
+//https://rancher.com/docs/rancher/v1.6/en/hosts/
